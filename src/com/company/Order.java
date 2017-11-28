460dbcdb4a1bff53a232d190d29a0a154fa8c004
Adrian Bernat <adrian_bernat@yahoo.com>
2017-11-27 18:41:42 -0800

    Fixed display

--- src/com/company/Order.java
+++ src/com/company/Order.java
@@ -1,6 +1,6 @@
 package com.company;
 
-abstract public class Order {
+public abstract class Order {
     //data fields
     protected Food[] contents_of_order;
     protected String restaurant_name;
@@ -8,9 +8,22 @@ abstract public class Order {
 
     //members
     Order(){
-        contents_of_order = null;
+        contents_of_order = new Food[10];
         restaurant_name = null;
         name_of_orderer = null;
     }
+
+    public void set_restaurant_name(String to_set){
+        restaurant_name = to_set;
+    }
+
+    public void set_orderer_name(String to_set){
+        name_of_orderer = to_set;
+    }
+
+    public void display(){
+        System.out.print("Order details: ");//probably use printf for this, display name of restaurant and person and then call food display
+    }
+
 
 }
